d2455d2aa3868ca31d336c86d0ac5bb59df54981
xiaol913 <xxlangdou@gmail.com>
2017-10-18 20:04:26 +0800

    second commit

--- js/index.js
+++ js/index.js
@@ -3,9 +3,10 @@ $(document).ready(function () {
         overlay = $('.overlay'),
         stbar1 = $('.st-bar-1'),
         stbar2 = $('.st-bar-2'),
-        stbar3 = $('.st-bar-3'),
         stbar4 = $('.st-bar-4'),
-        stscoll = $('.st-scroll');
+        stscoll = $('.st-scroll'),
+        above_page = $('.above_page_btn'),
+        next_page = $('.next_page_btn'),
         isClosed = false;
 
     trigger.click(function () {
@@ -14,7 +15,6 @@ $(document).ready(function () {
 
     stbar1.click(function () {
         stbar2.removeClass('active');
-        stbar3.removeClass('active');
         stbar4.removeClass('active');
         stbar1.addClass('active');
         stscoll.removeClass('scroll-2');
@@ -24,7 +24,6 @@ $(document).ready(function () {
     });
     stbar2.click(function () {
         stbar1.removeClass('active');
-        stbar3.removeClass('active');
         stbar4.removeClass('active');
         stbar2.addClass('active');
         stscoll.removeClass('scroll-1');
@@ -32,25 +31,30 @@ $(document).ready(function () {
         stscoll.removeClass('scroll-4');
         stscoll.addClass('scroll-2');
     });
-    stbar3.click(function () {
-        stbar2.removeClass('active');
-        stbar1.removeClass('active');
-        stbar4.removeClass('active');
-        stbar3.addClass('active');
-        stscoll.removeClass('scroll-2');
-        stscoll.removeClass('scroll-1');
-        stscoll.removeClass('scroll-4');
-        stscoll.addClass('scroll-3');
-    });
     stbar4.click(function () {
         stbar2.removeClass('active');
-        stbar3.removeClass('active');
         stbar1.removeClass('active');
         stbar4.addClass('active');
         stscoll.removeClass('scroll-2');
         stscoll.removeClass('scroll-3');
         stscoll.removeClass('scroll-1');
         stscoll.addClass('scroll-4');
+    });
+    next_page.click(function () {
+        stbar1.removeClass('active');
+        stbar4.removeClass('active');
+        stscoll.removeClass('scroll-2');
+        stscoll.removeClass('scroll-1');
+        stscoll.removeClass('scroll-4');
+        stscoll.addClass('scroll-3');
+    });
+    above_page.click(function () {
+        stbar1.removeClass('active');
+        stbar4.removeClass('active');
+        stscoll.removeClass('scroll-3');
+        stscoll.removeClass('scroll-1');
+        stscoll.removeClass('scroll-4');
+        stscoll.addClass('scroll-2');
     });
 
     function hamburger_cross() {
